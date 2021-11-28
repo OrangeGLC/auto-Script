978c2070f26f4b42d31df4874a91d5fab6d7707b
Sam,Jing <rong4916@foxmail.com>
2021-11-29 00:16:52 +0800

    [CHG][NORMAL]Update scrcpy launch cmd args.

--- auto-launch-scrcpy.sh
+++ auto-launch-scrcpy.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
-echo "=====scrcpy test===="
+logfile=/tmp/autoscrcpy.log
+logline=0
 while true
 do
-    adb devices
-    sleep 1
+    devNum=$[ $(adb devices |wc -l)-2]
+    if [ $devNum -ge 1 ]
+    then
+        echo "$(date) Find ${devNum} device" >> $logfile
+        echo "$(date) Launch scrcpy ..." >> $logfile
+        scrcpy -b 2M -m 1024 --max-fps 60 --window-x 0 --window-y 0 --always-on-top -w >> $logfile
+#    else
+#        echo "Can not find any devices!"
+    fi
+    if [ -e $logfile ]
+    then
+        logline=$[ $(cat $logfile|wc -l)]
+    fi
+    if [ $logline -gt 50000 ]
+    then
+        rm -rf $logfile
+        echo "$(date) too large log should be delete!"
+    fi
+    sleep 5
 done
+
